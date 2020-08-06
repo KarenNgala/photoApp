68c4392d97f7cba3602ea4774e1e172c5f5f15a2
KarenNgala <ngalalalu@gmail.com>
2020-08-07 02:31:24 +0300

    Configure templating

--- project_insta/settings.py
+++ project_insta/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'app_insta',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
