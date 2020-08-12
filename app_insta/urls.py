098361c2f84e6228644e7425e8f1c375612bc39b
KarenNgala <ngalalalu@gmail.com>
2020-08-12 23:38:57 +0300

    Add user profile link

--- app_insta/urls.py
+++ app_insta/urls.py
@@ -4,7 +4,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
-    url(r'^$', views.feed, name='feed')
+    url(r'^$', views.feed, name='feed'),
+    url(r'^profile/$', views.profile, name='profile'),
 ]
 
 if settings.DEBUG:
