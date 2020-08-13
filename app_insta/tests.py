e9b4cf48bb99f962aca203b91bf22e8a0892b0c1
KarenNgala <ngalalalu@gmail.com>
2020-08-13 15:14:40 +0300

    Add dynamic data to homepage

--- app_insta/tests.py
+++ app_insta/tests.py
@@ -8,24 +8,27 @@ from django.contrib.auth.models import User
 class ProfileTest(TestCase):
     ''' test class for Profile model'''
     def setUp(self):
+        ''' method called before each test case'''
         self.user = User.objects.create_user(username='Water')
 
     def tearDown(self):
+        ''' method to clear all setup instances after each test run '''
         self.user.delete()
 
     def test_profile_creation(self):
+        ''' method to test profile instance is created only once for each user '''
         self.assertIsInstance(self.user.profile, Profile)
         self.user.save()
         self.assertIsInstance(self.user.profile, Profile)
-
+        
 
 class TestImage(TestCase):
     ''' test class for image model '''
     def setUp(self):
-        ''' method to create Image instances to be called before each test case'''
+        ''' method called before each test case'''
         self.test_user = User(username='Linda', password='123')
         self.test_user.save()
-        self.test_profile = User.objects.last().profile
+        self.test_profile = self.test_user.profile
         self.test_profile.save()
 
         self.test_comment = Comment(name=self.test_profile, comment_body='Test comment', created_on=datetime.now())
@@ -43,8 +46,7 @@ class TestImage(TestCase):
         self.assertEqual(len(Image.objects.all()), 1)
 
     def tearDown(self):
-        ''' '''
+        ''' method to clear all setup instances after each test run '''
         self.test_user.delete()
-        Profile.objects.all().delete()
         Comment.objects.all().delete()
         Image.objects.all().delete()
--- app_insta/views.py
+++ app_insta/views.py
@@ -4,8 +4,7 @@ from .models import Image, Profile, Comment, Relation
 # Create your views here.
 def feed(request):
     pictures = Image.objects.all()
-
-    return render(request, 'index.html')
+    return render(request, 'index.html',{'pictures':pictures})
 
 def profile(request):
     return render(request, 'profile.html')
